166f4918d3c204e394f941007e0e3831a1c27975
AaronM <info@aamservices.uk>
2021-01-20 14:43:46 +0000

    Update validUserId.js

--- src/helpers/validUserId.js
+++ src/helpers/validUserId.js
@@ -1,13 +1,22 @@
 // eslint-disable-next-line no-unused-vars
- module.exports = (target = 'id') => {
+module.exports = (target = 'id') => {
   return async context => {
     try {
-      if (context.method.match('create')) {
-        context.data[target] = String(context.params.user._id);
-      } else if (context.method.match(/(remove|update|patch)/)) {
-        context.id = String(context.params.user._id);
-      } else {
-        context.params.query[target] = String(context.params.user._id);
+      //check if user has admin permission and that they want all results not just own
+      if (typeof context.params.user.permissions === 'undefined' ||
+        !context.params.user.permissions.includes('admin') ||
+        typeof context.params.query.show === 'undefined' ||
+        context.params.query.show !== 'all') {
+        if (context.method.match('create')) {
+          context.data[target] = String(context.params.user._id);
+        } else if (context.method.match(/(remove|update|patch)/)) {
+          context.id = String(context.params.user._id);
+        } else {
+          context.params.query[target] = String(context.params.user._id);
+        }
+      }
+      if (typeof context.params.user.show !== 'undefined') {
+        delete context.params.query.show;
       }
     } catch (err) {
       throw new Error('Invalid User');
